0498b4675636e92b6d7051aa1ecb8ba6c8e1b279
Danyllo <danyllo_mona@hotmail.com>
2023-06-12 20:03:37 -0300

    first

---
+++ home.php
@@ -0,0 +1,146 @@
+<!doctype html>
+<html lang="pt-br">
+    <head>
+        <!-- Meta Tags -->
+        <meta charset="iso-8859-1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+        <meta name="title" content="Intranet :: Interamérica"/>
+        <meta name="author" content="T.I. Interamérica - Ramal 208 - Email: fernando@escolainteramerica.com.br / raphael@escolainteramerica.com.br">
+        <meta name="copyright" content="® 2016" />
+        <meta name="distribution" content="global">
+        <!-- Título da Página -->
+        <title>Intramerica :: Intranet</title>
+        <!-- Favicon -->
+        <link rel="apple-touch-icon" sizes="152x152" href="../imagens/favicons/apple-touch-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="../imagens/favicons/apple-touch-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="../imagens/favicons/apple-touch-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="../imagens/favicons/apple-touch-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="../imagens/favicons/apple-touch-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="../imagens/favicons/apple-touch-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="../imagens/favicons/apple-touch-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="57x57" href="../imagens/favicons/apple-touch-icon-57x57.png">
+        <link rel="icon" type="image/png" href="../imagens/favicons/favicon-196x196.png" sizes="196x196">
+        <link rel="icon" type="image/png" href="../imagens/favicons/favicon-160x160.png" sizes="160x160">
+        <link rel="icon" type="image/png" href="../imagens/favicons/favicon-96x96.png" sizes="96x96">
+        <link rel="icon" type="image/png" href="../imagens/favicons/favicon-32x32.png" sizes="32x32">
+        <link rel="icon" type="image/png" href="../imagens/favicons/favicon-16x16.png" sizes="16x16">
+        <link rel="shortcut icon" href="../imagens/favicons/favicon.ico" />
+
+        <link rel="stylesheet" type="text/css" href="../plugins/frontend/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="../plugins/frontend/bootstrap-table/font-awesome/css/font-awesome.min.css">
+        <link rel="stylesheet" type="text/css" href="../css/index.css"/>
+
+        <script type="text/javascript" src="../plugins/frontend/jquery/jquery.min.js"></script>
+        <script type="text/javascript" src="../plugins/frontend/bootstrap/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="../plugins/frontend/moment-with-langs/moment-with-langs.min.js"></script>
+        <script type="text/javascript" src="../jscripts/geral.js"></script>
+    </head>
+    <body>
+        <div id="main" class="container-fluid">
+            <!-- Container Aplicação #Begin -->
+            <div class="row aplicacao">
+                <!-- Container Topo #Begin -->
+                <header class="col-xs-12 col-sm-12 col-md-12col-lg-12 topo">
+                    <!-- Login intranet -->
+                    <div class="divBotaoLogin">
+                        <a href="index.php" target="_BLANK">
+                            <img src="../imagens/index/icone-login.png" class="botaoLogin" />
+                        </a>
+                    </div>
+                </header>
+                <!-- navegacao max. 767px -->
+                <div class="mobile">
+                    <header class="mobile-topo">
+                       <!-- logo -->
+                        <section id="logo-mobile"><a href="" target="_self"><img src="../imagens/logos/logo-horizontal.svg" class="logo-mobile"></a></section>
+                        <section id="login-mobile"><a href="index.php" target="_BLANK"><img src="../imagens/icones/login.png" class="login-mobile"></a></section>
+                    </header>
+                </div>
+                <!-- Container Conteúdo #Begin -->
+                <div id="icones-index">
+                    <div class="row">
+                        <div class="control-input form-group col-xs-12 col-sm-12 col-md-4 col-lg-4" align="center">
+                            <a href="../../calendariointeramerica/week.php" target="_BLANK">
+                        		<div class="agendaTeatro" id="agendaTeatro">
+                                    <img class="agendaTeatroInicial" src="../imagens/index/icone-teatro.png" />
+                                    <img class="agendaTeatroOnHover" src="../imagens/index/icone-teatro2.png" />
+                        		</div>
+                    		</a>
+                        </div>
+                        <div class="control-input form-group col-xs-12 col-sm-12 col-md-4 col-lg-4" align="center">
+                            <a href="../../agendaprojetor1/week.php" target="_BLANK">
+                        		<div class="agendaProjetor" id="agendaProjetor">
+                                    <img class="agendaProjetorInicial" src="../imagens/index/icone-projetor.png" />
+                                    <img class="agendaProjetorOnHover" src="../imagens/index/icone-projetor2.png" />
+                        		</div>
+                    		</a>
+                        </div>
+                        <div class="control-input form-group col-xs-12 col-sm-12 col-md-4 col-lg-4" align="center">
+                            <a href="../../agendatedi/week.php" target="_BLANK">
+                        		<div class="agendaTedi" id="agendaTedi">
+                                    <img class="agendaTediInicial" src="../imagens/index/icone-tedi.png" />
+                                    <img class="agendaTediOnHover" src="../imagens/index/icone-tedi2.png" />
+                        		</div>
+                    		</a>
+                        </div>
+                    </div>
+                    
+                    <div class="row">
+                        <div class="control-input form-group col-xs-12 col-sm-12 col-md-4 col-lg-4" align="center">
+                            <a href="../../agendabiblioteca/week.php" target="_BLANK">
+                        		<div class="agendaBiblioteca" id="agendaBiblioteca">
+                                    <img class="agendaBibliotecaInicial" src="../imagens/index/icone-biblioteca.png" />
+                                    <img class="agendaBibliotecaOnHover" src="../imagens/index/icone-biblioteca2.png" />
+                        		</div>
+                    		</a>
+                        </div>
+                        <div class="control-input form-group col-xs-12 col-sm-12 col-md-4 col-lg-4" align="center">
+                            <a href="../../calendarioaniversarios/week.php" target="_BLANK">
+                        		<div class="agendaProfessor" id="agendaProfessor">
+                                    <img class="agendaProfessorInicial" src="../imagens/index/icone-professor.png" />
+                                    <img class="agendaProfessorOnHover" src="../imagens/index/icone-professor2.png" />
+                        		</div>
+                    		</a>
+                        </div>
+                        <div class="control-input form-group col-xs-12 col-sm-12 col-md-4 col-lg-4" align="center">
+                            <a href="../../agendamaker/week.php" target="_BLANK">
+                        		<div class="agendaMaker" id="agendaMaker">
+                                    <img class="agendaMakerInicial" src="../imagens/index/icone-maker.png" />
+                                    <img class="agendaMakerOnHover" src="../imagens/index/icone-maker2.png" />
+                        		</div>
+                    		</a>
+                        </div>
+                    </div>
+                    
+                    <div class="row">
+                        <div class="control-input form-group col-xs-12 col-sm-12 col-md-4 col-lg-4" align="center">
+                            <a href="../../intramerica/admin/recibo-avulso.php" target="_BLANK">
+                        		<div class="recibo" id="recibo">
+                                    <img class="reciboInicial" src="../imagens/index/icone-recibo.png" />
+                                    <img class="reciboOnHover" src="../imagens/index/icone-recibo2.png" />
+                        		</div>
+                    		</a>
+                        </div>
+                        <div class="control-input form-group col-xs-12 col-sm-12 col-md-4 col-lg-4" align="center">
+                            <a href="../../intramerica/admin/acompanhamento-matricula.php?action=acessoProfessores" target="_BLANK">
+                        		<div class="acompanhamentoMatricula" id="acompanhamentoMatricula">
+                                    <img class="acompanhamentoMatriculaInicial" src="../imagens/index/icone-matricula.png" />
+                                    <img class="acompanhamentoMatriculaOnHover" src="../imagens/index/icone-matricula2.png" />
+                        		</div>
+                    		</a>
+                        </div>
+                        <div class="control-input form-group col-xs-12 col-sm-12 col-md-4 col-lg-4" align="center">
+                            <a href="../../intramerica/public/index.php" target="_BLANK">
+                        		<div class="reuniaoIndividualEtapa" id="reuniaoIndividualEtapa">
+                                    <img class="reuniaoIndividualEtapaInicial" src="../imagens/index/icone-reuniao.png" />
+                                    <img class="reuniaoIndividualEtapaOnHover" src="../imagens/index/icone-reuniao2.png" />
+                        		</div>
+                    		</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
